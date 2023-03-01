5cec37f90267b75cf05488a021c975aa4257f1bf
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-03-01 14:41:50 +0300

    Labs 5.18 + 6.2zh.

--- labs/5.18/src/main.f90
+++ labs/5.18/src/main.f90
@@ -12,37 +12,67 @@
 
 program Sampling
    use Environment
-   ! TODO: перенеси обработку и ввод/вывод в отдельные модули.
-   !       ввод/вывод осуществляй из файла, как обычно.
-   ! use Sampling_Mod
-   ! use Sampling_IO
+   use IEEE_Arithmetic  ! Для ieee_quiet_nan.
    implicit none
 
-   character(*), parameter  :: INPUT_FILE  = "../data/input.txt", OUTPUT_FILE = "output.txt"
+   character(*), parameter  :: INPUT_FILE  = "../data/input.txt",  &
+                               OUTPUT_FILE = "output.txt",         &
+                               fmt_int     = '(a, T7, "= ", i0)',  &
+                               fmt_real    = '(a, T7, "= ", f0.2)'
    ! p и q - заданные значения, по которым будет осуществляться выборка.
+   ! mean_val - среднее арифметическое.
+   real(R_)                 :: p, q, mean_val
    ! N - количество первых значений выборки, по которым будет находиться среднее арифметическое.
-   integer(I_), parameter   :: p = 3, q = 5, N = 6
+   integer(I_)              :: In, Out, sizeA, N
    ! Исходный массив.
-   integer(I_), allocatable :: A(:)
-   ! Выборка элементов массива A, удовлетворяющая условию p <= ai <= q.
-   integer(I_), allocatable :: GoodA(:)
-   integer(I_)              :: In, Out, sizeA
+   real(R_), allocatable :: A(:)
 
    open(file=INPUT_FILE, newunit=In)
-      read(In, *) sizeA
+      read(In, *) sizeA, p, q, N
       allocate(A(sizeA))
       read(In, *) A
    close(In)
 
-   GoodA = pack(A, p <= A .and. A <= q)
+   open(file=output_file, encoding=E_, newunit=out)
+      write(Out, fmt_int)  "sizeA", sizeA
+      write(Out, fmt_real) "p", p
+      write(Out, fmt_real) "q", q
+      write(Out, fmt_int)  "N", N
+      write(Out, '(a, T7, "= (", ' // sizeA // 'f6.2, ")")') "A", A
+      write(Out, *)
+   close(In)
 
-   open(file=OUTPUT_FILE, encoding=E_, newunit=Out, position="rewind")
+   mean_val = Mean(A, p, q, N)
+
+   open(file=OUTPUT_FILE, encoding=E_, newunit=Out, position="append")
       ! Если в выборке менее N чисел.
-      if(size(GoodA) < N) then
+      if(IEEE_Is_NaN(mean_val)) then
          write(Out, *) "В выборке менее "// N //" чисел."
       else
-         write(Out, *) sum(GoodA(1:N)) / real(N, R_)  ! Вычисляем среднее арифметическое.
+         write(Out, fmt_real) "mean", mean_val
       end if
    close(Out)
+
+contains
+   real(R_) pure function Mean(A, p, q, N)
+      real(R_),    intent(in) :: A(:)
+      real(R_),    intent(in) :: p, q
+      integer(I_), intent(in) :: N
+      ! Выборка элементов массива A, удовлетворяющая условию p <= ai <= q.
+      real(R_), allocatable   :: GoodA(:)
+
+      ! Выборка элементов массива A, удовлетворяющая условию p <= ai <= q.
+      GoodA = pack(A, p <= A .and. A <= q)
+
+      ! Если в выборке менее N чисел.
+      if(size(GoodA) < N) then
+         ! Возвращаем NaN.
+         Mean = IEEE_Value(Mean, IEEE_Quiet_NaN)
+         return
+      endif
+
+      ! Вычисляем среднее арифметическое.
+      Mean = sum(GoodA(1:N)) / real(N, R_)
+   end function Mean
 
 end program Sampling
--- labs/6.2zh/src/main.f90
+++ labs/6.2zh/src/main.f90
@@ -2,8 +2,8 @@
 ! Лабораторная работа №6, вариант 6.2 (ж).
 
 ! Задача: вычислить сумму членов рядов, представляющих
-!  значения следующих фукнций (суммирование производить
-!  до тех пор, пока отношение текущего члена ряда к 
+!  значения следующих функций (суммирование производить
+!  до тех пор, пока отношение текущего члена ряда к
 !  накопленной сумме не станет меньше заданной величины
 !  RELERR):
 !  ln(x) = (x-1)/x + ((x-1)^2)/(2x^2) + ((x-1)^3)/(3x^3) + ... ,
@@ -21,10 +21,115 @@
 
 program LnX
    use Environment
-   ! use LnX_Mod
-   ! use LnX_IO
+   use IEEE_Arithmetic  ! Для ieee_quiet_nan.
    implicit none
 
-   character(*), parameter   :: INPUT_FILE  = "../data/input.txt", &
-                                OUTPUT_FILE = "output.txt"
+   character(*), parameter :: input_file  = "../data/input.txt", &
+                              output_file = "output.txt", &
+                              fmt         = '(a, T15, "= ", e13.6)'
+   integer(I_)             :: in, out
+   real(R_)                :: x, log_x
+
+   open(file=input_file, newunit=in)
+      read(In, *) x
+   close(In)
+
+   open(file=output_file, encoding=E_, newunit=out)
+      write(Out, fmt) 'x', x
+      write(Out, *)
+   close(In)
+
+   ! Вычисляем значение натурального логарифма нашей функцией.
+   log_x = Ln_X(x)
+
+   open(file=output_file, encoding=E_, newunit=out, position="append")
+      ! Выводим наш ln(x), встроенный ln(x) и разницу между ними (ошибку).
+      write (Out, fmt) "ln(x)",         log_x
+      write (Out, fmt) "Fortran ln(x)", log(x)
+      write (Out, fmt) "Error",         log_x - log(x)
+   close(Out)
+
+contains
+   ! Реализация, в которой мы вычисляем текущие
+   !  x и (x - 1) относительно предыдущих.
+   real(R_) pure function Ln_X(x)
+      real(R_), intent(in) :: x
+      real(R_)             :: dividend, divisor, x_current, x_minus_1, old_ln_x
+      integer(I_)          :: n
+
+      ! Если x <= 0.5, то возвращаем NaN,
+      !  т.к. по условию задачи x > 0.5.
+      if (x <= 0.5) then
+         Ln_X = IEEE_Value(x, IEEE_Quiet_NaN)
+         return
+      end if
+
+      x_current = x  ! Повышает свою степень с каждой итерацией.
+
+      ! Числитель первого члена ряда: x - 1.
+      x_minus_1 = x_current - 1
+      dividend = x_minus_1
+
+      ! Знаменатель первого члена ряда: x.
+      divisor = x_current
+      ! Переменная для знаменателя, инкрементируемая с каждой итерацией.
+      n = 1
+
+      ! Вычисляем первый член ряда ln(x).
+      Ln_X = dividend / divisor
+
+      ! Цикл с постусловием: пока сумма не перестанет меняться.
+      do
+         ! Сохраняем текущую сумму для нашего постусловия (см. выше).
+         old_ln_x  = Ln_X
+
+         ! Числитель: (x - 1)^n.
+         dividend = dividend * x_minus_1
+
+         ! Знаменатель: n * x^n.
+         n = n + 1
+         x_current = x_current * x
+         divisor = n * x_current
+
+         ! Вычисление текущего члена ряда �� прибавление его к общей сумме.
+         Ln_X = Ln_X + dividend / divisor
+
+         ! Если сумма перестала меняться, выходим.
+         if (old_ln_x == Ln_X) exit
+      end do
+   end function Ln_X
+
+   ! Реализация, которая не удовлетворяет условию
+   !  "очередной член вычислять относительно предыдущего",
+   !  но которая, на мой взгляд, читаемее.
+   real(R_) pure function Ln_X_Impl(x)
+      real(R_), intent(in) :: x
+      real(R_)             :: x_minus_1, old_ln_x
+      integer(I_)          :: n
+
+      ! Если x <= 0.5, то возвращаем NaN,
+      !  т.к. по условию задачи x > 0.5.
+      if (x <= 0.5) then
+         Ln_X_Impl = IEEE_Value(x, IEEE_Quiet_NaN)
+         return
+      end if
+
+      ! Ln_X изначально равен нулю.
+      Ln_X_Impl = 0.0_R_
+      ! Вычисляем заранее x - 1, чтобы не вычислять каждый раз.
+      x_minus_1 = x - 1.0_R_
+      ! От итерации с итерации мы на самом деле добавляем к предыдущей сумме:
+      !  ((x - 1)^n) / (n * x^n), где n [1; +inf).
+      ! По этой причине присваиваем n значение 1 и будем инкрементировать его.
+      n = 1
+
+      ! Цикл с постусловием: пока сумма не перестанет меняться.
+      do
+         old_ln_x  = Ln_X_Impl
+         Ln_X_Impl = Ln_X_Impl + x_minus_1**n / (n * x**n)
+         if (old_ln_x == Ln_X_Impl) exit
+         n = n + 1
+      end do
+   end function Ln_X_Impl
+
 end program LnX
