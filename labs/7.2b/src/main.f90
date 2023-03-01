5527bdc888c22ded38a1b07b324c2b6f46f1abdc
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-03-01 16:07:29 +0300

    Lab 7.2b: Start!

--- labs/7.2b/src/main.f90
+++ labs/7.2b/src/main.f90
@@ -13,10 +13,42 @@
 
 program SortWithPackMask
    use Environment
-   ! use Sort_With_Pack_Mask_Mod
-   ! use Sort_With_Pack_Mask_IO
    implicit none
 
-   character(*), parameter   :: INPUT_FILE  = "../data/input.txt", &
-                                OUTPUT_FILE = "output.txt"
+   character(*), parameter   :: input_file  = "../data/input.txt", &
+                                output_file = "output.txt",        &
+                                fmt_int     = '(a, T14, "= ", i0)'
+   integer(I_)               :: in, out, array_size
+   real(R_), allocatable     :: array(:)
+
+   open(file=input_file, newunit=in)
+      read(in, *) array_size
+      allocate(array(array_size))
+      read(in, *) array
+   close(in)
+
+   open(file=output_file, encoding=E_, newunit=out)
+      write(out, fmt_int)  "array_size", array_size
+      write(out, '(a, T14, "= (", ' // array_size // 'f6.2, ")" )') "array", array
+      write(out, *)
+   close(out)
+
+   call SortNegatives(array)
+
+   open(file=output_file, encoding=E_, newunit=out, position="append")
+      write(out, '(a, T14, "= (", ' // array_size // 'f6.2, " )")') "sorted array", array
+   close(out)
+
+contains
+   pure subroutine SortNegatives(array)
+      real(R_), intent(out) :: array(:)
+      logical :: mask(size(array))
+
+      mask = (array > 0)
+      array = [Pack(array=array, mask=.not. mask), Pack(array=array, mask=mask)]
+      ! TODO: Сортировка положительных элементов в порядке убывания (от большего к меньшему).
+      ! ... some sort algorithm ...
+      ! ... maybe from his examples ...
+   end subroutine SortNegatives
+
 end program SortWithPackMask
