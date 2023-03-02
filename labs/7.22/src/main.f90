e91490c5d31e2b0ce8112d8c070b4213d30e048d
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-03-02 13:55:32 +0300

    Lab 7.22: Done!

--- labs/7.22/src/main.f90
+++ labs/7.22/src/main.f90
@@ -9,10 +9,30 @@
 
 program MinMaxDiff
    use Environment
-   ! use Min_Max_Diff_Mod
-   ! use Min_Max_Diff_IO
    implicit none
 
-   character(*), parameter   :: INPUT_FILE  = "../data/input.txt", &
-                                OUTPUT_FILE = "output.txt"
+   character(*), parameter   :: input_file  = "../data/input.txt", output_file = "output.txt"
+   integer(I_)               :: in, n, m, i, out
+   real(R_), allocatable     :: matrix(:, :)
+   real(R_)                  :: difference
+
+   open(file=input_file, newunit=in)
+      read(in, *) n, m
+      allocate(matrix(n, m))
+      ! Хранение в памяти по столбцам.
+      read(in, *) (matrix(i, :), i = 1, n)
+   close(in)
+
+   open(file=output_file, encoding=E_, newunit=out)
+      write(out, '(2(a, " = ", i0/))') "n", n, "m", m
+      write(out, "(a)") "matrix:"
+      write(out, "(" // m // "f7.2)") (matrix(i, :), i = 1, n)
+      write(out, *)
+   close(out)
+
+   difference = MaxVal(matrix) - MinVal(matrix)  ! max - min.
+
+   open(file=output_file, encoding=E_, newunit=out, position="append")
+      write(out, '(a, " = ", f0.2)') "difference (max - min)", difference
+   close(out)
 end program MinMaxDiff
