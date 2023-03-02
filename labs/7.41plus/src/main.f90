e4c354baba330fbaf04a139d848de60fe87ed63d
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-03-02 14:31:15 +0300

    Lab 7.41plus: Done???

--- labs/7.41plus/src/main.f90
+++ labs/7.41plus/src/main.f90
@@ -10,10 +10,42 @@
 
 program ArrayConverter
    use Environment
-   ! use Array_Converter_Mod
-   ! use Array_Converter_IO
    implicit none
 
-   character(*), parameter   :: INPUT_FILE  = "../data/input.txt", &
-                                OUTPUT_FILE = "output.txt"
+   character(*), parameter   :: input_file  = "../data/input.txt", output_file = "output.txt"
+   real(R_), allocatable     :: matrix_2d(:, :)
+   real(R_), allocatable     :: matrix_1d(:)
+   integer(I_)               :: in, n, m, i, out
+
+   open(file=input_file, newunit=in)
+      read(in, *) n, m
+      allocate(matrix_2d(n, m))
+      ! Хранение в памяти по столбцам.
+      read(in, *) (matrix_2d(i, :), i = 1, n)
+   close(in)
+
+   open(file=output_file, encoding=E_, newunit=out)
+      write(out, '(2(a, " = ", i0/))') "n", n, "m", m
+      write(out, "(a)") "matrix_2d:"
+      write(out, "(" // m // "f7.2)") (matrix_2d(i, :), i = 1, n)
+      write(out, *)
+   close(out)
+
+   ! Сформируется по столбцам, то есть:
+   !  [ 1 2 3 ]
+   !  [ 4 5 6 ]
+   !            => [ 1 4 2 5 3 6 ].
+   matrix_1d = [matrix_2d]
+
+   ! Сформируется по строкам, то есть:
+   !  [ 1 2 3 ]
+   !  [ 4 5 6 ]
+   !            => [ 1 2 3 4 5 6 ].
+   ! matrix_1d = [(matrix_2d(i, :), i = 1, n)]
+
+   open(file=output_file, encoding=E_, newunit=out, position="append")
+      write(out, "(a)") "matrix_1d:"
+      write(out, "(" // size(matrix_1d) // "f7.2)") matrix_1d
+   close(out)
+
 end program ArrayConverter
