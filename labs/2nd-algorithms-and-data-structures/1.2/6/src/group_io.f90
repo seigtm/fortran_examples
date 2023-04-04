ac28947cd0062133a907cab636fae1796a1e1184
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-04-04 16:28:31 +0300

    Lab 1.2.6: started.

---
+++ labs/2nd-algorithms-and-data-structures/1.2/6/src/group_io.f90
@@ -0,0 +1,6 @@
+module group_io
+   use environment
+   implicit none
+
+   ! code goes brrr...
+end module group_io
---
+++ labs/2nd-algorithms-and-data-structures/1.2/6/src/group_process.f90
@@ -0,0 +1,7 @@
+module group_process
+   use environment
+   use group_io
+   implicit none
+
+   ! code goes brrr...
+end module group_process
