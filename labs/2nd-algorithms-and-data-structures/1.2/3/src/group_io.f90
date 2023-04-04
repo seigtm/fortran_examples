b72815a60144532db8645a0153dc61c62ebd813a
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-04-04 15:58:47 +0300

    Lab 1.2.3: fixed indent.

--- labs/2nd-algorithms-and-data-structures/1.2/3/src/group_io.f90
+++ labs/2nd-algorithms-and-data-structures/1.2/3/src/group_io.f90
@@ -27,15 +27,15 @@ contains
       record_length = (surname_length + initials_length + 2)*CH_ + R_
       ! Открытие файла для записи данных в неформатированном виде.
       open(file=data_file, form='unformatted', newunit=out, access='direct', recl=record_length)
-         ! Формат для чтения из файла.
-         format = '(4(a, 1x), f5.2)'
-         ! Чтение данных из входного файла и запись в неформатированный файл.
-         do i = 1, students_count
-            read(in, format, iostat=io) stud
-            call handle_io_status(io, "reading formatted class list, line " // i)
-            write(out, iostat=io, rec=i) stud
-            call handle_io_status(io, "creating unformatted file with class list, record " // i)
-         end do
+      ! Формат для чтения из файла.
+      format = '(4(a, 1x), f5.2)'
+      ! Чтение данных из входного файла и запись в неформатированный файл.
+      do i = 1, students_count
+         read(in, format, iostat=io) stud
+         call handle_io_status(io, "reading formatted class list, line " // i)
+         write(out, iostat=io, rec=i) stud
+         call handle_io_status(io, "creating unformatted file with class list, record " // i)
+      end do
       ! Закрытие файлов.
       close(in)
       close(out)
@@ -51,14 +51,14 @@ contains
       record_length = ((surname_length + initials_length + 2)*CH_ + R_) * students_count
       ! Открытие файла в двоичном режиме, с указанием размера записи и количества записей в файле.
       open(file=data_file, form='unformatted', newunit=in, access='direct', recl=record_length)
-         ! Чтение записи из файла с указанным номером и сохранение ее в массиве group.
-         read(in, iostat=io, rec=1) group
-         ! Проверка статуса операции чтения из файл.
-         call handle_io_status(io, "reading unformatted class list")
+      ! Чтение записи из файла с указанным номером и сохранение ее в массиве group.
+      read(in, iostat=io, rec=1) group
+      ! Проверка статуса операции чтения из файл.
+      call handle_io_status(io, "reading unformatted class list")
       ! Закрытие файла.
       close(in)
    end function read_students_list
- 
+
    ! Вывод списка класса.
    subroutine output_students_list(output_file, group, list_name, position)
       character(*),  intent(in) :: output_file, list_name, position  ! Имена файла и списка, позиция.
@@ -68,14 +68,14 @@ contains
 
       ! Открытие файла на запись с указанием позиции курсора в файле.
       open(file=output_file, encoding=E_, position=position, newunit=out)
-         ! Запись названия списка в файл.
-         write(out, '(/a)') list_name
-         ! Определение формата вывода данных о студентах.
-         format = '(4(a, 1x), f5.2)'
-         ! Запись данных о студентах в файл в соответствии с заданным форматом.
-         write(out, format, iostat=io) group
-         ! Проверка статуса операции записи в файл.
-         call handle_io_status(io, "writing " // list_name)
+      ! Запись названия списка в файл.
+      write(out, '(/a)') list_name
+      ! Определение формата вывода данных о студентах.
+      format = '(4(a, 1x), f5.2)'
+      ! Запись данных о студентах в файл в соответствии с заданным форматом.
+      write(out, format, iostat=io) group
+      ! Проверка статуса операции записи в файл.
+      call handle_io_status(io, "writing " // list_name)
       ! Закрытие файла.
       close(out)
    end subroutine output_students_list
