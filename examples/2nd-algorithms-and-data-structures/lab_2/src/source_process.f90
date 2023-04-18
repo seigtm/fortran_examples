81a52c97d153b8f87f24d8cf477edf965c9df016
seigtm <gh@seig.ru>
2023-04-18 20:49:48 +0300

    Notes in examples 1.6 and 2.

--- examples/2nd-algorithms-and-data-structures/lab_2/src/source_process.f90
+++ examples/2nd-algorithms-and-data-structures/lab_2/src/source_process.f90
@@ -20,7 +20,7 @@ contains
       if (InitialCode%String == ModdedCode%String) then
          ! Если остались ещё строки, то переход к следующей.
          if (Associated(InitialCode%next)) then
-            DiffCode => Diff_Codes(InitialCode%next, ModdedCode%Next)
+            DiffCode => Diff_Codes(InitialCode%next, ModdedCode%Next)  ! Почему на 23 строке Initial_code идёт с next, а на 33 строке – без него?
          ! В противном случае если остались строки в модифицированном файле, то добавление их в список.
          else if (Associated(ModdedCode%next)) then
             ! Запись всех строк оставшейся части ModdedCode.
@@ -30,7 +30,18 @@ contains
       else
          allocate (DiffCode)
          DiffCode%String = CH__"++ " // ModdedCode%String
-         DiffCode%next => Diff_Codes(InitialCode, ModdedCode%Next)
+         DiffCode%next => Diff_Codes(InitialCode, ModdedCode%Next)  ! Почему на 23 строке Initial_code идёт с next, а на 33 строке – без него?
+         ! Потому что на 23 строке мы рекурсивно вызываем эту же функцию, если существуют следующие строки в коде.
+         ! А вот на 33 строке не совсем та же ситуация: мы выполняем код выше только в случае, если строки не совпали:
+         !     1     &&  1 =>      (строки    совпали, игнорируем).
+         !     2     &&  2 =>      (строки    совпали, игнорируем).
+         !     3     &&  3 =>      (строки    совпали, игнорируем).
+         ! [ пусто ] &&  4 => ++ 4 (строки не совпали, дописываем).
+         !     5     &&  5 =>      (строки    совпали, игнорируем).
+         ! Если бы мы не делали этой махинации, а сдвинулись на Next ещё и в InitialCode, то мы бы просто пропустили сравнение
+         !  строчек "5", что привело бы к неправильному ответу в данном случае.
+         ! В общем, мы не сдвигаемся как раз таки для случая, когда у нас в ModdedCode добавилась новая строчка, которой не было в
+         !  InitialCode.
       end if
    end function Diff_Codes
 
